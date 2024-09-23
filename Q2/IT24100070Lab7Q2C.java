4c9ea676ed9bf02ea0ca8202f91859d2bc8ad7ac
IT24100070 <it24100070@my.sliit.lk>
2024-09-23 20:52:23 +0530

    Q2C

---
+++ Q2/IT24100070Lab7Q2C.java
@@ -0,0 +1,16 @@
+public class IT24100070Lab7Q2C {
+
+ public static void main(String[] args) {
+		
+    for(int rowCount = 5; rowCount >= 1; rowCount--)
+    {
+
+      for(int columnCount = 1; columnCount <= rowCount; columnCount++)
+       {
+       System.out.print(rowCount);
+      
+       }
+    System.out.println();
+    }
+  }
+}
